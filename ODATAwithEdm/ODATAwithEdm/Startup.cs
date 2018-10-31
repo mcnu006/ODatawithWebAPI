66d6fc40ed065a76a2f90d4c27f50518a3a94cad
sreenivasa.rm <sreenivasa.rm@sonata-software.com>
2018-10-31 11:36:42 +0530

    OData

---
+++ ODATAwithEdm/ODATAwithEdm/Startup.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNet.OData.Routing.Conventions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.OData;
+using Microsoft.OData.Edm;
+using Microsoft.OData.UriParser;
+using ODATAwithEdm.Models;
+
+namespace ODATAwithEdm
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        private static IEdmModel GetEdmModel(IServiceProvider serviceProvider)
+        {
+            var builder = new ODataConventionModelBuilder(serviceProvider);
+
+           // ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Book>("Books");
+            builder.EntitySet<Press>("Presses");
+            return builder.GetEdmModel();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddOData();
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            var model = GetEdmModel(app.ApplicationServices);
+            //app.UseMvc();
+            app.UseMvc(routeBuilder =>
+            {
+                routeBuilder.Select().Expand().Filter().OrderBy().MaxTop(100).Count();
+
+                // To support queries of enum, It will convert property Name to value
+                // Reference : https://github.com/OData/WebApi/issues/904
+                routeBuilder.MapODataServiceRoute(
+                    "api",
+                    "api",
+                    containerBuilder => containerBuilder
+                        .AddService(Microsoft.OData.ServiceLifetime.Singleton, sp => model)
+                        .AddService<IEnumerable<IODataRoutingConvention>>(Microsoft.OData.ServiceLifetime.Singleton, sp => ODataRoutingConventions.CreateDefaultWithAttributeRouting("api", routeBuilder))
+                        .AddService(Microsoft.OData.ServiceLifetime.Singleton, typeof(ODataUriResolver), sp => new StringAsEnumResolver { EnableCaseInsensitive = true }));
+
+                routeBuilder.EnableDependencyInjection();
+            });
+        }
+
+    }
+}
